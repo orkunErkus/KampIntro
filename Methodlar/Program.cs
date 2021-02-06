34425985cde27120ad30c1e9fe4b17cf497f4553
Orkun Erkuş <maniacq@msn.com>
2021-02-06 22:44:58 +0300

    16.01.2021 DersSonu

---
+++ Methodlar/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Methodlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Urun urun1 = new Urun { };
+            urun1.Adi = "elma";
+            urun1.Aciklama = "amasya elması";
+            urun1.Fiyati = 15;
+
+            Urun urun2 = new Urun { };
+            urun2.Fiyati = 80;
+            urun2.Adi = "karpuz";
+            urun2.Aciklama = "diyarbakır";
+
+            Urun[] urunler = new Urun[] { urun1,urun2};
+
+            foreach (Urun urun in urunler)
+            {
+                Console.WriteLine(urun.Fiyati);
+                Console.WriteLine(urun.Aciklama);
+                Console.WriteLine(urun.Adi);
+                Console.WriteLine("------------");
+            }
+
+            Console.WriteLine("-----methotlar--------!");
+
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("armut" , "yeşil" , 12);
+            sepetManager.Ekle2("elma" , "kırmızı" , 11);
+            sepetManager.Ekle2("karpuz" , "diyarbakır" , 13);
+        }
+    }
+}
---
+++ Methodlar/SepetManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Methodlar
+{
+    class SepetManager
+    {
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Eklendi : " + urun.Adi);
+        }       
+        
+        public void Ekle2(string Adi , string Aciklama , double Fiyati)
+        {
+            Console.WriteLine("Eklendi : " + Adi);
+        }
+    }
+}
