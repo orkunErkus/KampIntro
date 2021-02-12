a8150f8a57fe0470e775a9849a6d7b0aedc5df70
Orkun Erkuş <maniacq@msn.com>
2021-02-12 22:37:59 +0300

    27.01.2021

---
+++ SQLQuery4.sql
@@ -0,0 +1,5 @@
+﻿Select ProductName ÜrünAdı, 
+Sum([Order Details].Quantity * [Order Details].UnitPrice) AS KazanılanToplamMiktar 
+from [Order Details]
+Right join Products on [Order Details].ProductID = Products.ProductID
+group by ProductName
